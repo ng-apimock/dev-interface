62e452caba3472d77e22845af7f9a3569a9a4f2d
Mischa Dasberg <mischa@dasberg.nl>
2018-10-23 00:02:56 +0200

    fix(test-server): add body-parser to filter requests to ngapimock

--- test/server.js
+++ test/server.js
@@ -2,21 +2,24 @@ const apimock = require('@ng-apimock/core');
 const connect = require('connect');
 const path = require('path');
 const serveStatic = require('serve-static');
+const bodyParser = require('body-parser');
 const app = connect();
-const testApplicationMocksDirectory = path.join(require.resolve('@ng-apimock/test-application'), '..', '..', 'mocks');
 const testMocksDirectory = path.join(__dirname, 'mocks');
 
-apimock.processor.process({src: testApplicationMocksDirectory});
 apimock.processor.process({src: testMocksDirectory});
-
+app.use(bodyParser.json());
 // use ng-apimock to test the web interface.
-app.use(function (request, response, next) {
-    if (request.url.startsWith('/ngapimock/') &&
-        request.url !== '/ngapimock/recordings' &&
-        request.url !== '/ngapimock/actions') {
-        request.url = `/ngapimock-intercept/${request.url.substring(11)}`;
+app.use((request, response, next) => {
+    if (request.url.startsWith('/ngapimock/')) {
+        if (request.url !== '/ngapimock/recordings' &&
+            request.url !== '/ngapimock/init' &&
+            !(request.url === '/ngapimock/actions' && request.body.action === 'record')) {
+            request.url = `/ngapimock-intercept/${request.url.substring(11)}`;
+        }
+        next();
+    } else {
+        next();
     }
-    next();
 });
 app.use(apimock.middleware);
 app.use('/dev-interface/', serveStatic(path.join(__dirname, '..', 'dist'), {index: ['index.html']}));
