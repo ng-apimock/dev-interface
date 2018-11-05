a64c9403c4ec1954624acf067b8ecf3aaf125dcb
Mischa Dasberg <mischa@dasberg.nl>
2018-11-05 22:22:03 +0100

    feat(filter-mocks-pipe): use filter mocks pipe

--- src/app/mocks/overview/overview.module.ts
+++ src/app/mocks/overview/overview.module.ts
@@ -3,17 +3,25 @@ import {NgModule} from '@angular/core';
 import {OverviewComponent} from './overview.component';
 import {OverviewRowModule} from './overview-row.module';
 import {RouterModule, Routes} from '@angular/router';
+import {FilterMocksPipe} from './filter.mocks.pipe';
+import {FormsModule} from '@angular/forms';
 
 export const routes: Routes = [{ path: 'mocks', component: OverviewComponent }];
 
 @NgModule({
     imports: [
         CommonModule,
+        FormsModule,
         OverviewRowModule,
         RouterModule.forChild(routes),
     ],
-    declarations: [OverviewComponent],
-    exports: [OverviewComponent]
+    declarations: [
+        OverviewComponent,
+        FilterMocksPipe
+    ],
+    exports: [
+        OverviewComponent
+    ]
 })
 export class OverviewModule {
 }
