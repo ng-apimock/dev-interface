f231e393a0cb2841d9bc818722c5c3c659b94504
Mischa Dasberg <mischa@dasberg.nl>
2018-11-05 22:20:58 +0100

    feat(filter-mocks-pipe): add filter mocks pipe

---
+++ src/app/mocks/overview/filter.mocks.pipe.spec.ts
@@ -0,0 +1,26 @@
+import {FilterMocksPipe} from './filter.mocks.pipe';
+
+describe('FilterMocksPipe', () => {
+    let pipe: FilterMocksPipe;
+
+    describe('transform', () => {
+        beforeEach(() => {
+            pipe = new FilterMocksPipe();
+        });
+
+        it('gets the matching mocks', () => {
+            const matches = pipe.transform([{name:'some'}, {name:'thing'}], 'so');
+            expect(matches).toEqual([{name:'some'}]);
+        });
+
+        it('returns [] when no mocks are provided', () => {
+            const matches = pipe.transform(undefined, 'so');
+            expect(matches).toEqual([]);
+        });
+
+        it('returns all the mocks when no search text is provided', () => {
+            const matches = pipe.transform([{name:'some'}, {name:'thing'}], undefined);
+            expect(matches).toEqual([{name:'some'}, {name:'thing'}]);
+        });
+    });
+});
---
+++ src/app/mocks/overview/filter.mocks.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({name: 'filter'})
+export class FilterMocksPipe implements PipeTransform {
+    transform(mocks: any[], searchText: string): any[] {
+        if(!mocks) return [];
+        if(!searchText) return mocks;
+        searchText = searchText.toLowerCase();
+        return mocks.filter( (mock) => mock.name.toLowerCase().includes(searchText));
+    }
+}
