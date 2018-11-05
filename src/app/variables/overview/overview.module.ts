4f3e348ec78114594e9895bf178d431feaecbafd
Mischa Dasberg <mischa@dasberg.nl>
2018-11-05 22:22:45 +0100

    feat(filter-variables-pipe): use filter variables pipe

--- src/app/variables/overview/overview.module.ts
+++ src/app/variables/overview/overview.module.ts
@@ -4,18 +4,22 @@ import {OverviewComponent} from './overview.component';
 import {RouterModule, Routes} from '@angular/router';
 import {OverviewRowModule} from './overview-row.module';
 import {OverviewFooterRowModule} from './overview-footer-row.module';
+import {FilterVariablesPipe} from './filter.variables.pipe';
+import {FormsModule} from '@angular/forms';
 
 export const routes: Routes = [{ path: 'variables', component: OverviewComponent }];
 
 @NgModule({
     imports: [
         CommonModule,
+        FormsModule,
         OverviewFooterRowModule,
         OverviewRowModule,
         RouterModule.forChild(routes),
     ],
     declarations: [
-        OverviewComponent
+        OverviewComponent,
+        FilterVariablesPipe
     ],
     exports: [
         OverviewComponent
